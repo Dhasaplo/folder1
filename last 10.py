e0073b916705eea38e6562d764d8ca2fe8822701
Dhasaplo <42028209+Dhasaplo@users.noreply.github.com>
2018-08-23 12:31:44 +0530

    Create last 10.py

---
+++ last 10.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=0
+
+if (a/10)>0:
+  b=b+1
+  if (a/10)>0:
+    b=b+1
+    if(a/10)>0:
+      b=b+1
+      if(a/10)>0:
+        b=b+1
+        if(a/10)>0:
+          b=b+1
+  print(b)
+else :
+  print(b)
